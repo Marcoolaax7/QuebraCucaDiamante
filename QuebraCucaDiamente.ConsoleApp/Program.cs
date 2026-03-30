a063ef33fd399c177193a14ef933876a01d4718a
Marcoolaax7 <marcooliver2@yahoo.com>
2026-03-30 13:36:41 -0300

    QuebraCucaDiamante

---
+++ QuebraCucaDiamente.ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace QuebraCuca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite um numero inteiro Ímpar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroDigitado % 2 != 0)
+            {
+                int metade = (numeroDigitado - 1) / 2;
+                int espaco = metade;
+                int xs = 1;
+
+
+                for (int i = 0; i <= metade; i++)
+                {
+                    for (int i2 = 0; i2 < espaco; i2++)
+                        Console.Write(" ");
+
+                    for (int i2 = 0; i2 < xs; i2++)
+                        Console.Write("X");
+
+                    Console.WriteLine();
+
+                    xs += 2;
+                    espaco--;
+                }
+
+                xs -= 4;
+                espaco += 2;
+
+                for (int i = 0; i < metade; i++)
+                {
+                    for (int i2 = 0; i2 < espaco; i2++)
+                        Console.Write(" ");
+
+                    for (int i2 = 0; i2 < xs; i2++)
+                        Console.Write("X");
+
+                    Console.WriteLine();
+
+                    xs -= 2;
+                    espaco++;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Numero Invalido! Digite um numero Ímpar.");
+                Console.ReadLine();
+            }
+        }
+        
+    }
+}
